57cfb74cb997a5485ecb2f69d690a3f2e4f1bb15
Shivam Morya <155000765+Shivammaurya69@users.noreply.github.com>
2024-02-27 14:23:57 +0530

    Add files via upload

---
+++ ex_5/Ques_b.java
@@ -0,0 +1,14 @@
+package com.ex_5;
+
+import java.util.Scanner;
+
+public class Ques_b {
+    public static void main(String[] args) {
+        double base, exponent;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the numbers.");
+        base= sc.nextDouble();
+        exponent= sc.nextDouble();
+        System.out.println(Math.pow(base, exponent));
+    }
+}
---
+++ ex_5/Ques_f.java
@@ -0,0 +1,6 @@
+package com.ex_5;
+
+public class Ques_f {
+    public static void main(String[] args) {
+    }
+}
